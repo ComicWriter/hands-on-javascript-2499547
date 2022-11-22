1f63ddafd4673781af7274a44fb3b1dc15bb71c2
ComicWriter <jrmackinnon@hotmail.com>
2022-11-22 00:32:47 +0000

    Iterate thru data.js images. Output a card for each.

--- 01_03b/components/cardlist.js
+++ 01_03b/components/cardlist.js
@@ -1,5 +1,21 @@
 import Card from "./Card.js";
 
-const Cardlist = (data) => {};
+const cardListItem = (ImageData) => {
+  return `
+    <li class="cardlist_item">
+      ${Card(ImageData)}
+    </li>
+  `;
+};
+
+const Cardlist = (data) => {
+  return `
+    <section class="cardlist">
+      <ul class="cardlist_list">
+        ${data.map((ImageData) => cardListItem(ImageData)).join("")}
+      </ul>
+    </section>
+  `;
+};
 
 export default Cardlist;
