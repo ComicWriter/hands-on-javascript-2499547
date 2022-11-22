0f5e02e20cca72122481dbd131863fce7fbf64e4
ComicWriter <jrmackinnon@hotmail.com>
2022-11-22 02:06:39 +0000

    Fixed spacing and typos

--- 01_03b/components/Cardlist.js
+++ 01_03b/components/Cardlist.js
@@ -1,19 +1,17 @@
 import Card from "./Card.js";
 
 const cardListItem = (imgData) => {
-  return `
-    <li class="cardlist_item">
-      ${Card(imgData)}
-    </li>
-  `;
+  return `<li class="cardlist__item">
+            ${Card(imgData)}
+        </li>`;
 };
 
 const Cardlist = (data) => {
   return `
     <link href="components/cardlist.css" rel="stylesheet" />
     <section class="cardlist">
-      <ul class="cardlist_list">
-        ${data.map((imgData) => cardListItem(imgData)).join("")}
+      <ul class="cardlist__list">
+          ${data.map((imgData) => cardListItem(imgData)).join("")}
       </ul>
     </section>
   `;
